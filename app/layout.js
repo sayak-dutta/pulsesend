3bc8e159f1bb0c00d2bb300f0a0d21a9304ed97e
Sayak Dutta <sayak@Sayaks-MacBook-Air.local>
2023-07-27 03:33:11 +0530

    vercel analytics integration

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,10 @@ export default function RootLayout({ children }) {
 			<head>
 				<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 			</head>
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
